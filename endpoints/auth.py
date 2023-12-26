14e94a5bdf6e9181cfd40b869e2a04e89ad4780b
Faakhir30 <zahidfaakhir@gmail.com>
2023-12-26 16:44:06 +0500

    added reqs

--- endpoints/auth.py
+++ endpoints/auth.py
@@ -32,7 +32,7 @@ def signup():
             elif role == 'employee':
                 new_user = Employee(user_name=name, email=email, password=hashed_password, contact=contact)
             else:
-                return jsonify({'error': 'Invalid role'}), 400
+                return jsonify({'error': 'Invalid role', 'status':400}), 400
 
             # Adding the user to the database
             db.session.add(new_user)
@@ -44,7 +44,7 @@ def signup():
             error_info = e.orig.diag.message_primary if e.orig.diag.message_primary else str(e.orig)
             return jsonify({'error': error_info}), 500
         except TypeError as e:
-            return jsonify({'error': str(e)}), 400
+            return jsonify({'error': str(e), 'status':400}), 400
         
 @auth_blueprint.route('/login', methods=['POST'])
 def login():
@@ -54,10 +54,10 @@ def login():
             password = request.json['password']
             user = User.query.filter_by(email=email).first()
             if user is None:
-                return jsonify({'error': 'Invalid email or password'}), 400
+                return jsonify({'error': 'Invalid email or password', 'status':400}), 400
             hashed_password = user.password
             if not bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8')):
-                return jsonify({'error': 'Invalid email or password'}), 400
+                return jsonify({'error': 'Invalid email or password', 'status':400}), 400
 
             # Creating the response object
             response = {
@@ -74,6 +74,7 @@ def login():
             token = jwt.encode(response["data"], secret_key, algorithm='HS256')
             response['data'].pop('created_at')
             response['data']['token'] = token
+            response['status'] = 200
             return jsonify(response), 200
 
         except IntegrityError as e:
